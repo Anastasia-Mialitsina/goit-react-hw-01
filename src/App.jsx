b4a87aece5069d870fb58d2f827938c2f887f1a6
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-02-25 19:23:00 +0200

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-// import React from 'react';
 import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import userData from "./userData.json";
 import friends from "./friends.json";
+import transactions from "./transactions.json";
+
 import "./App.css";
 
 const App = () => {
@@ -20,8 +22,13 @@ const App = () => {
       <div className="friend-list">
         <FriendList friends={friends} />
       </div>
+      <div className="transaction-history">
+        <TransactionHistory items={transactions} />{" "}
+        {/* Здесь передаем данные как пропс */}
+      </div>
     </div>
   );
 };
 
 export default App;
+
